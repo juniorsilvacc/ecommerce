d444ce5c8b65a796b04557784a48af4a4c8bf329
juniorsilvacc <juniorsilvacc@hotmail.com>
2023-12-28 19:51:15 -0300

    development

--- resources/js/components/admin/partials/categories/CategoriesComponent.vue
+++ resources/js/components/admin/partials/categories/CategoriesComponent.vue
@@ -1,49 +1,12 @@
 <template>
     <div>
-        <table class="table table-dark">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nome</th>
-                    <th width="100">Ações</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr :v-for="(category, index) in categories.data" :key="index">
-                    <td>{{ category.id }}</td>
-                    <td>{{ category.name }}</td>
-                    <td>{{ category.id }}</td>
-                </tr>
-            </tbody>
-        </table>
+        <h1>Lista de Categorias</h1>
     </div>
 </template>
 
 <script>
-    import axios from "axios";
-
     export default {
-        created () {
-            this.loadCategories()
-        },
-        data () {
-            return {
-                categories: {
-                    data: []
-                },
-            }
-        },
-        methods: {
-            loadCategories () {
-                axios.get("/api/v1/categories")
-                    .then(response => {
-                        this.categories = response
-                    })
-                    .catch(errors => {
-                        console.log(errors)
-                    })
-            }
-        }
+
     }
 </script>
 
